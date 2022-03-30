21955dade7dfe6e7e503b1413a078f7177bcc6c5
I Putu Pawesi Siantika <pawesi.siantika@zettabyte.life>
2022-03-30 13:52:27 +0800

    Added PH sensor

--- IO_Mapping.h
+++ IO_Mapping.h
@@ -1,6 +1,10 @@
 /* This file contains pin connection from device to microcontroller */
 
+#ifndef IO_MAPPING_H
+#define IO_MAPPING_H
 /* Sensors */
 /* pH */
 #define PIN_PH A1
-/
+
+
+#endif
---
+++ Senso_PH.ino
@@ -0,0 +1,90 @@
+/*
+  # This sample code is used to test the pH meter V1.1.
+  # Editor : YouYou
+  # Date   : 2014.06.23
+  # Ver    : 1.1
+  # Product: analog pH meter
+  # SKU    : SEN0161
+*/
+
+#define Offset 42.56            //deviation compensate
+#define LED 13
+#define samplingInterval 20
+#define printInterval 800
+#define ArrayLenth  40    //times of collection
+#define uart  Serial
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex = 0;
+
+
+void setupSensorPH(void)
+{
+  pinMode(LED, OUTPUT);
+  uart.begin(9600);
+  uart.println("pH meter experiment!");    //Test the uart monitor
+}
+void readSensorPH(void)
+{
+  static unsigned long samplingTime = millis();
+  static unsigned long printTime = millis();
+  static float pHValue, voltage;
+  if (millis() - samplingTime > samplingInterval)
+  {
+    pHArray[pHArrayIndex++] = analogRead(PIN_PH);
+    if (pHArrayIndex == ArrayLenth)pHArrayIndex = 0;
+    voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
+    pHValue = -19.185* voltage + Offset;
+    samplingTime = millis();
+  }
+  if (millis() - printTime > printInterval)  //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
+  {
+    uart.print("Voltage:");
+    uart.print(voltage, 2);
+    uart.print("    pH value: ");
+    uart.println(pHValue, 2);
+    digitalWrite(LED, digitalRead(LED) ^ 1);
+    printTime = millis();
+  }
+}
+
+
+
+double avergearray(int* arr, int number) {
+  int i;
+  int max, min;
+  double avg;
+  long amount = 0;
+  if (number <= 0) {
+    uart.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if (number < 5) { //less than 5, calculated directly statistics
+    for (i = 0; i < number; i++) {
+      amount += arr[i];
+    }
+    avg = amount / number;
+    return avg;
+  } else {
+    if (arr[0] < arr[1]) {
+      min = arr[0]; max = arr[1];
+    }
+    else {
+      min = arr[1]; max = arr[0];
+    }
+    for (i = 2; i < number; i++) {
+      if (arr[i] < min) {
+        amount += min;      //arr<min
+        min = arr[i];
+      } else {
+        if (arr[i] > max) {
+          amount += max;  //arr>max
+          max = arr[i];
+        } else {
+          amount += arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount / (number - 2);
+  }//if
+  return avg;
+}
