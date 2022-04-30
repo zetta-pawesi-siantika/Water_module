48bee50dc96c1d79d6b4e23077c2db241ae3714f
I Putu Pawesi Siantika <pawesi.siantika@zettabyte.life>
2022-04-30 19:01:56 +0800

    created Mechanical_Setting.h

--- Main.ino
+++ Main.ino
@@ -1,16 +1,11 @@
 #include <Wire.h>
 #include "IO_Mapping.h"
 #include "Data_Capture.h"
+#include "Mechanical_Setting.h"
 
 #define V_REF_5V 5.0
 #define ADC_RESOLUTION 1024.0
-#define DELAY_EMPTYING_SEAWATER 17000 // second
-#define DELAY_EMPTYING_PUREWATER 11000 // second
-#define DELAY_SEAWATER_FULLFILL 4000// second
-#define DELAY_PUREWATER_FILLING 5500// second
-#define EMPTYING_SEAWATER_DEG 80 // degree
-#define EMPTYING_PUREWATER_DEG 120 // degree
-#define SETTLE_SERVO_DEG 0 // degree 
+
 
 // preprocessor write here --> it enabels or disables features
 #define MECHANICAL
---
+++ Mechanical_Setting.h
@@ -0,0 +1,15 @@
+/* Setup for delay for pumps and servo position */
+
+#ifndef MECHANICAL_SETTING_H
+#define MECHANICAL_SETTING_H
+
+
+#define DELAY_EMPTYING_SEAWATER 17000 // second
+#define DELAY_EMPTYING_PUREWATER 11000 // second
+#define DELAY_SEAWATER_FULLFILL 4000// second
+#define DELAY_PUREWATER_FILLING 5500// second
+#define EMPTYING_SEAWATER_DEG 80 // degree
+#define EMPTYING_PUREWATER_DEG 120 // degree
+#define SETTLE_SERVO_DEG 0 // degree 
+
+#endif
